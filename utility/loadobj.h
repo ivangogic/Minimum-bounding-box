03f97b8a3bc3ec66cbda59080f95b32568cfd335
Ivan Gogic <ivangogic@protonmail.com>
2026-09-20 06:25:58 +0200

    style: clang-format

--- utility/loadobj.h
+++ utility/loadobj.h
@@ -1,11 +1,7 @@
 #ifndef MINIMUM_BOUNDING_BOX_LOADOBJ_H
 #define MINIMUM_BOUNDING_BOX_LOADOBJ_H
 
-bool loadObj(
-        const std::string & path,
-        std::vector<glm::vec3> & out_vertices,
-        std::vector<glm::vec3> & out_normals,
-        std::vector<glm::vec3> & unique_vertices
-);
+bool loadObj(const std::string &path, std::vector<glm::vec3> &out_vertices, std::vector<glm::vec3> &out_normals,
+             std::vector<glm::vec3> &unique_vertices);
 
-#endif //MINIMUM_BOUNDING_BOX_LOADOBJ_H
+#endif // MINIMUM_BOUNDING_BOX_LOADOBJ_H
